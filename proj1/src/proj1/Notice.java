ed8a08574fd6efbcc0eec83ef93dbc385a6277ff
imagine9977 <imagine9977@naver.com>
2024-04-02 17:57:24 +0900

    첫 복사

--- proj1/src/proj1/Notice.java
+++ proj1/src/proj1/Notice.java
@@ -1,5 +1,8 @@
 package proj1;
 
 public class Notice {
-
+	private String ttype;
+	private int no;
+	private String route;
+	private String comment;
 }
--- proj1/src/proj1/Qna.java
+++ proj1/src/proj1/Qna.java
@@ -1,5 +1,8 @@
 package proj1;
 
 public class Qna {
-
+	private String ttype;
+	private int no;
+	private String route;
+	private String comment;
 }
